7c46dbc4a2b38c10092ef057472380292584e2ef
golden <magic.lixin@gmail.com>
2017-03-04 22:02:17 +0800

    MMMM

--- cp_ios_libs.sh
+++ cp_ios_libs.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-cp ./.build_ios/lib/libUrho3D.a ../Test/proj/VisioninDemo/VisioninDemo/urho3d/
-cp ./.build_ios/Source/ThirdParty/FreeType/Debug-iphoneos/libFreeType.a ../Test/proj/VisioninDemo/VisioninDemo/urho3d/
-cp ./.build_ios/Source/ThirdParty/JO/Debug-iphoneos/libJO.a ../Test/proj/VisioninDemo/VisioninDemo/urho3d/
-cp ./.build_ios/Source/ThirdParty/LZ4/Debug-iphoneos/libLZ4.a ../Test/proj/VisioninDemo/VisioninDemo/urho3d/
-cp ./.build_ios/Source/ThirdParty/PugiXml/Debug-iphoneos/libPugiXml.a ../Test/proj/VisioninDemo/VisioninDemo/urho3d/
-cp ./.build_ios/Source/ThirdParty/SDL/Debug-iphoneos/libSDL.a ../Test/proj/VisioninDemo/VisioninDemo/urho3d/
-cp ./.build_ios/Source/ThirdParty/StanHull/Debug-iphoneos/libStanHull.a ../Test/proj/VisioninDemo/VisioninDemo/urho3d/
+src_dir=./.build_ios
+dest_dir=../UrhoDemo/VisioninDemo/VisioninDemo/urho3d/
+
+cp ${src_dir}/lib/libUrho3D.a ${dest_dir}
+cp ${src_dir}/Source/ThirdParty/FreeType/Debug-iphoneos/libFreeType.a ${dest_dir}
+cp ${src_dir}/Source/ThirdParty/JO/Debug-iphoneos/libJO.a ${dest_dir}
+cp ${src_dir}/Source/ThirdParty/LZ4/Debug-iphoneos/libLZ4.a ${dest_dir}
+cp ${src_dir}/Source/ThirdParty/PugiXml/Debug-iphoneos/libPugiXml.a ${dest_dir}
+cp ${src_dir}/Source/ThirdParty/SDL/Debug-iphoneos/libSDL.a ${dest_dir}
+cp ${src_dir}/Source/ThirdParty/StanHull/Debug-iphoneos/libStanHull.a ${dest_dir}
 
 sync
